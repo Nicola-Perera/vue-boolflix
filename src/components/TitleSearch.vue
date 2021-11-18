31bbaf3861e6cf73281a10f1a0c68749f2cf3925
Nicola-Perera <nicola.perera96@mail.com>
2021-11-18 17:24:01 +0100

    complete milestone 1

--- src/components/TitleSearch.vue
+++ src/components/TitleSearch.vue
@@ -1,20 +1,20 @@
 <template>
-<!-- componente per l'input dell'utente -->
+  <!-- componente per l'input dell'utente -->
   <div id="titleSearch">
-
     <!-- titolo inserito dall'utente -->
-    <input type="text" name="searchByTitle" v-model="title">
+    <input type="text" name="searchByTitle" v-model="title" />
 
     <button name="search" @click="getValue()">Search</button>
 
     <div class="search_results">
       <CardLayout
-      v-for="movie in movies"
+        v-for="movie in movies"
         :title="movie.title"
         :original_title="movie.original_title"
         :original_language="movie.original_language"
         :vote_average="movie.vote_average"
-        :key="movie.id"/>
+        :key="movie.id"
+      />
     </div>
   </div>
 </template>
@@ -31,26 +31,24 @@ export default {
   data () {
     return {
       erroe: '',
+      title: '',
       movies: []
     }
   },
 
-  mounted () {
-    axios
-      .get('https://api.themoviedb.org/3/search/movie?api_key=8132756f90b9196d3a74d67879dedc3c&query=spiderman')
-      .then((response) => {
-        console.log(this.movies)
-        this.movies = response.data.results
-        console.log(this.movies)
-      })
-      .catch((Error) => {
-        console.log(Error, 'ERRORE!')
-        this.error = `ERRORE ${Error}`
-      })
-  },
   methods: {
     getValue () {
-      console.log(this.name.value)
+      axios
+        .get(
+          `https://api.themoviedb.org/3/search/movie?api_key=8132756f90b9196d3a74d67879dedc3c&query=${this.title}`
+        )
+        .then((response) => {
+          this.movies = response.data.results
+        })
+        .catch((Error) => {
+          console.log(Error, 'ERRORE!')
+          this.error = `ERRORE ${Error}`
+        })
     }
   }
 }
