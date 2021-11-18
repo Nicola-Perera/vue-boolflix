ca336d8b767c2429f0f036042413277095d3065e
Nicola-Perera <nicola.perera96@mail.com>
2021-11-18 11:15:51 +0100

    Initial commit: run project and create main structure

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+
+<!-- BOOLFLIX -->
+<!-- sito di distribuzione di film e serie tv -->
+  <div id="app">
+
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss">
+
+</style>
