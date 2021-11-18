2689930da0d6adaa9a10b0db45998840f946f43a
Nicola-Perera <nicola.perera96@mail.com>
2021-11-18 14:42:13 +0100

    get reponse from server and show movies on console

--- src/components/TitleSearch.vue
+++ src/components/TitleSearch.vue
@@ -6,10 +6,30 @@
 </template>
 
 <script>
-import axios from axios
+import axios from 'axios'
 
 export default {
-  name: 'titleSearch'
+  name: 'titleSearch',
+  data () {
+    return {
+      erroe: '',
+      movies: []
+    }
+  },
+
+  mounted () {
+    axios
+      .get('https://api.themoviedb.org/3/search/movie?api_key=8132756f90b9196d3a74d67879dedc3c&query=avatar')
+      .then((response) => {
+        console.log(this.movies)
+        this.movies = response.data.results
+        console.log(this.movies.)
+      })
+      .catch((Error) => {
+        console.log(Error, 'ERRORE!')
+        this.error = `ERRORE ${Error}`
+      })
+  }
 }
 </script>
 
