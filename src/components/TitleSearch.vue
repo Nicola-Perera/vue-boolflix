725ab243a717f1a149e82cc6f6bffc2e0954afac
Nicola-Perera <nicola.perera96@mail.com>
2021-11-19 11:49:44 +0100

    add flag key to each object of the array

--- src/components/TitleSearch.vue
+++ src/components/TitleSearch.vue
@@ -8,10 +8,11 @@
 
     <div class="search_results">
       <CardLayout
+        v-model="results"
         v-for="movie in movies"
-        :title="movie.title"
+        :title='movie.title'
         :original_title="movie.original_title"
-        :original_language="movie.original_language"
+        :original_language='movie.original_language + " ciao" '
         :vote_average="movie.vote_average"
         :key="movie.id"
       />
@@ -32,6 +33,7 @@ export default {
     return {
       erroe: '',
       title: '',
+      results: '',
       movies: []
     }
   },
@@ -43,7 +45,18 @@ export default {
           `https://api.themoviedb.org/3/search/movie?api_key=8132756f90b9196d3a74d67879dedc3c&query=${this.title}`
         )
         .then((response) => {
+          // ricevo i risultati della ricerca e li aggiungo alla mia lista
           this.movies = response.data.results
+
+          // alla lista restituita aggiungo la chiave con la bandiera corrispondente alla lingua originale ad ogni oggetto
+          this.movies.forEach(element => {
+            const languageFlag = {}
+            const key = 'flag'
+            const value = 'bandiera'
+            languageFlag[key] = value
+            element.push(languageFlag)
+          })
+          console.log(this.movies[1].original_language)
         })
         .catch((Error) => {
           console.log(Error, 'ERRORE!')
